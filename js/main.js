dacf5be3248940926efd9eb7c939827600a2e1e2
urbancsokgy <urbancsokgy@gmail.com>
2020-11-19 15:33:25 +0100

    Kész

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 const balloon=document.querySelectorAll('.balloon');
+// Random color 
+function setBalloon(){
+ 
+  let randomColor = Math.floor(Math.random()*16777215).toString(16);
+  balloon.forEach(element => {
+    randomColor = Math.floor(Math.random()*16777215).toString(16);
+    element.style.backgroundColor= "#" + randomColor;
+  });
+}; setBalloon();
+
 
 (function bclick(){
   balloon.forEach(element => {
     element.addEventListener('click', (event) =>{
-      console.log('hello');
       element.classList.add('boom');
+      element.innerHTML='<span>OOPs</span>';
+      element.style.backgroundColor= 'white';
     })
   });
 })();
+
